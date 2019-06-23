f9fa8e0abb50fc8a1c5b86ea2c1fd8ecd7e36024
Ivan Posca Doria <ivanpdoria@gmail.com>
2019-06-23 12:16:48 -0300

    Initial version with robot dynamics and friction

---
+++ parametros.m
@@ -0,0 +1,32 @@
+%% Clear environment
+clc;
+clear all;
+close all;
+
+%% Parametros fixos
+% m1 = 2.2;
+% m2 = 2.2;
+% mp = 2;
+% 
+% L1 = 0.6;
+% L2 = 0.6;
+% l1 = 0.3;
+% l2 = 0.3;
+% I1 = 0.066;
+% I2 = 0.066;
+% g = 9.81;
+
+%% Parametros variaveis
+% Passo de integracao
+passo_integracao = 1e-4;
+t_sim = 30;
+% Condicoes iniciais
+x1_0 = 0;
+x2_0 = 0;
+x3_0 = 0;
+x4_0 = 0;
+% Atrito
+Kat = .3;
+%% Executa Simulacao
+open_system('projeto_2016/estados')
+simOut = sim('projeto_2016');
