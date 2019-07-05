d7288f314aba8a3091d62e686695776ac50153ee
Ivan Posca Doria <ivanpdoria@gmail.com>
2019-07-05 00:05:54 -0300

    Final version

--- geracao.m
+++ geracao.m
@@ -8,8 +8,8 @@ close all;
 passo_integracao = 1e-4;
 t_sim = 10;
 % Condicoes iniciais
-x1_0 = 0;
-% x1_0 = -0.5857;
+x1_0 = -pi;
+% x1_0 = 0;
 x2_0 = 0;
 x3_0 = 0;
 % x3_0 = 1.1714
@@ -69,13 +69,16 @@ lista_tempos_quadrado = 0:intervalo:tempo_d;
 
 % lista_quad_x
 % lista_quad_y
-%%
+%% Gera circulo
 
 L1 = 0.6;
 L2 = 0.6;
+r = sqrt(L1^2+L2^2);
 
 circulo = 360;
-num_pontos = 180;
+num_pontos = 90;
+
+lim_teste = 90;
 
 tempo_d = t_sim;
 intervalo = tempo_d/num_pontos;
@@ -83,7 +86,7 @@ lista_tempos = 0:intervalo:tempo_d;
 
 theta_passo = circulo/num_pontos;
 
-lista_thetas = [0:theta_passo:circulo];
+lista_thetas = [-180:theta_passo:180];
 
 raio = 1;
 lista_posx = [];
@@ -101,22 +104,26 @@ end
 lista_theta1 = [];
 lista_theta2 = [];
 
-% Confere Cinematica Inversa
+% Cinematica Inversa
+close all; figure; hold on;
+%for i = 1:num_pontos+1
 for i = 1:num_pontos+1
     xd = lista_posx(i);
     yd = lista_posy(i);
 
-    aux1 = (xd^2+yd^2-L1^2-L2^2)/(2*L1*L2);
-    theta2 = acos(aux1);
+    cos_beta = (L1^2+L2^2-r^2)/(2*L1*L2);
+    sin_beta = sqrt(1-cos_beta^2);
+    beta = atan2(real(sin_beta),real(cos_beta));
+    theta2 = pi-beta;
     lista_theta2(i) = theta2;
-    aux2_num = yd*(L1+L2*cosd(theta2))-xd*L2*sind(theta2);
-    aux2_den = xd*(L1+L2*cosd(theta2))+yd*L2*sind(theta2);
-    aux2 = aux2_num/aux2_den;
-%     theta1 = atan2d(L2*cosd(theta2),L2*sind(theta2))
-%     theta1 = atan2(aux2_num, aux2_den);
-    theta1 = atan(aux2);
-    lista_theta1(i) = theta1;
 
+    alpha = atan2(yd,xd);
+    cos_gama = (r^2+L1^2-L2^2)/(2*r*L1);
+    sin_gama = sqrt(1-cos_gama^2); % deu valor complexo
+    gama = atan2(real(sin_gama),real(cos_gama));
+    theta1 = alpha-gama;
+    lista_theta1(i) = theta1;
+    
     px1 = L1*cosd(theta1);
     py1 = L1*sind(theta1);
     px2 = px1+L2*cosd(theta1+theta2);
@@ -124,11 +131,10 @@ for i = 1:num_pontos+1
     r = sqrt(px2^2+py2^2);
     L1_calc = sqrt(px1^2+py1^2);
     L2_calc = sqrt((px2-px1)^2+(py2-py1)^2);
-%     plot([0 px1],[0 py1]);
-%     plot([px1 px2],[py1 py2]);
+
 end
 
-% plot(lista_posx,lista_posy);
+plot(lista_posx,lista_posy);
 
 %% Executa Simulacao
 open_system('projeto_2016/compara_in_out')
